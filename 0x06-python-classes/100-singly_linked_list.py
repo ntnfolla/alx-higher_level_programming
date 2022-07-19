86c24cd0c3af1e8857194d7455b9c40d9eeb7a89
ntnfolla <ntnfolla@gmail.com>
2022-07-19 11:48:06 -0700

    bonus 1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -69,6 +69,7 @@ class SinglyLinkedList:
                 tmp = tmp.next_node
             new.next_node = tmp.next_node
             tmp.next_node = new
+
     def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
         values = []
