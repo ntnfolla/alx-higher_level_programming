b5a993f9dffe1950087bd9cf8bd9193026fd04e0
ntnfolla <ntnfolla@gmail.com>
2022-08-02 06:17:28 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -16,7 +16,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Get a dictionary representation of the Student."""
         return self.__dict__
